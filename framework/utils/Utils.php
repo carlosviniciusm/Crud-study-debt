9ae7dd05ca5b95b939c692673feba6631018aa3e
Carlos Vinicius <cvmm321@gmail.com>
2021-08-08 20:51:51 -0300

    rcv38 - Add functions to import css and js dynamically

--- framework/utils/Utils.php
+++ framework/utils/Utils.php
@@ -16,5 +16,27 @@ class Utils
     {
         return preg_replace("/[^0-9]/", "", $sValue);
     }
+
+    /**
+     * Import javascripts files dynamically
+     * @param string $sFile
+     * @param string $sModule
+     */
+    public static function importJs(string $sFile, string $sModule = ''): void
+    {
+        $sModule = !empty($sModule) ? $sModule : 'general';
+        echo '../public/js/'.$sModule.'/'.$sFile.'.js';
+    }
+
+    /**
+     * Import css files dynamically
+     * @param string $sFile
+     * @param string $sModule
+     */
+    public static function importCss(string $sFile, string $sModule = ''): void
+    {
+        $sModule = !empty($sModule) ? $sModule : 'general';
+        echo '../public/css/'.$sModule.'/'.$sFile.'.css';
+    }
     
 }
