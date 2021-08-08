e0f169b67d8afb4ffb532109bc0e997fc7296265
Carlos Vinicius <cvmm321@gmail.com>
2021-08-08 18:32:46 -0300

    rcv31 - Add function to test save debtor with existent cpf

--- tests/DebtorTest.php
+++ tests/DebtorTest.php
@@ -40,6 +40,35 @@ class DebtorTest extends TestCase
     }
 
     /**
+     * Test save same CPF/CNPJ
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveSameCpf()
+    {
+        $aDados = [
+            'name' => 'Carlos Teste',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '01234567890',
+            'birthdate' => '15/01/1996',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $oDebtor = Debtor::createFromRequest($aDados);
+        $this->expectException(PDOException::class);
+        $oDebtor->save();
+        $oDebtorDAO = new DebtorDAO();
+        $oDebtor = $oDebtorDAO->findByCpfCnpj('01234567890');
+        $this->assertTrue(!is_null($oDebtor->getId()));
+    }
+
+    /**
      * Test delete fake debtor
      */
     public function testDebtorDelete()
