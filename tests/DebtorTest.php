b590c8d67cb63aba7fcdb98e33b9564f52b3d146
Carlos Vinicius <cvmm321@gmail.com>
2021-08-08 18:33:22 -0300

    rcv32 - Add functions to test filling validation of fields

--- tests/DebtorTest.php
+++ tests/DebtorTest.php
@@ -83,4 +83,279 @@ class DebtorTest extends TestCase
         $oDebtor = $oDebtorDAO->findByCpfCnpj('01234567890');
         $this->assertFalse(!is_null($oDebtor->getId()));
     }
+
+    /**
+     * Test save name empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveNameEmpty()
+    {
+        $aDados = [
+            'name' => '',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '15/01/1996',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save email empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveEmailEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => '',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '15/01/1996',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save cpf/cnpj empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveCpfCnpjEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '',
+            'birthdate' => '15/01/1996',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save birthdate empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveBirthdateEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save phone number empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSavePhoneNumberEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save zipcode empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveZipcodeEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save address empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveAddressEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => '',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save number empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveNumberEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save neighborhood empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveNeighborhoodEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Complemento Teste',
+            'neighborhood' => '',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save city empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveCityEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => '',
+            'state' => 'SE'
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Test save state empty
+     * @throws InvalidAttributeException
+     */
+    public function testDebtorSaveStateEmpty()
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm121@gmail.com',
+            'cpfcnpj' => '1234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste 2',
+            'neighborhood' => 'Bairro Teste 2',
+            'city' => 'Aracaju',
+            'state' => ''
+        ];
+
+        $this->expectException(InvalidAttributeException::class);
+        $oDebtor = Debtor::createFromRequest($aDados);
+    }
 }
