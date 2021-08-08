d1dd851fb37077a4760be43969768b86851cc8fc
Carlos Vinicius <cvmm321@gmail.com>
2021-08-08 16:23:47 -0300

    rcv05 - Add header, footer and menu

--- src/view/menu.php
+++ src/view/menu.php
@@ -1,1 +1,25 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div>
+        <span style="color: white">Gerenciador de dívidas | </span>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Devedores
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="debtor/register.php">Cadastrar</a>
+            <a class="dropdown-item" href="#">Listar</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dívidas
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Cadastrar</a>
+            <a class="dropdown-item" href="#">Listar</a>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    </div>
+</nav>
