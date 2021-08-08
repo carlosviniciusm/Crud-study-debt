0a8490371a23250b6d36cd6ee6a4e4a47f9d2210
Carlos Vinicius <cvmm321@gmail.com>
2021-08-08 16:43:08 -0300

    rcv15 - Add method to find debtor using cpf or cnpj

--- src/dao/DebtorDAO.php
+++ src/dao/DebtorDAO.php
@@ -1,7 +1,55 @@
 <?php
 namespace src\dao;
 
+use DateTimeImmutable;
+use Exception;
+use framework\system\Connection;
+use framework\utils\constants\TrueOrFalse;
+use PDO;
+use src\model\debtor\Debtor;
+use http\Exception\RuntimeException;
+use PDOException;
+
+/**
+ * Class DebtorDAO
+ * @package src\dao
+ */
 class DebtorDAO
 {
+
+    /**
+     * Find debtor using cpf or cnpj
+     * @param string $sCpfCnpj
+     * @return Debtor
+     */
+    public function findByCpfCnpj(string $sCpfCnpj): Debtor
+    {
+        $sSql = "SELECT * FROM dbr_debtor WHERE dbr_cpf_cnpj = ?";
+
+        try {
+            $oConnection = Connection::getConnection();
+
+            $stmt = $oConnection->prepare($sSql);
+
+            if (!$stmt) {
+                throw new PDOException();
+            }
+
+            $stmt->bindParam(1, $sCpfCnpj);
+
+            $stmt->execute();
+            $aDebtor = $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new RuntimeException("Error when consulting debtor.", 500, $e);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        if (!$aDebtor) {
+            return new Debtor();
+        }
+
+        return Debtor::createFromArray($aDebtor);
+    }
 
 }
