41aa681f98725a5f14c67a8e3e9787d23f5b44ca
Carlos Vinicius <cvmm321@gmail.com>
2021-08-08 16:22:17 -0300

    rcv04 - Refactor index.php using Router class to route

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
 <?php
 
+use framework\system\Router;
+
 require_once "vendor/autoload.php";
 
-if (!empty($_REQUEST)) {
-    $sController = "receiveit\Controller\\" . $_REQUEST['controller'] . "Controller";
-    if (class_exists($sController)) {
-        $oEntity = new $sController($_REQUEST);
-        $sAction = $_REQUEST['action'];
-
-        if (empty($sAction)) {
-            $sAction = "index";
-        }
-
-        $aData = [];
-        if (method_exists($sController, $sAction)) {
-            $aData['request'] = $_REQUEST;
-            $oEntity->$sAction($aData);
-        }
-    }
-} else {
-    include_once "home.php";
-}
+$oRouter = new Router();
+$oRouter->init();
+$oRouter->route();
