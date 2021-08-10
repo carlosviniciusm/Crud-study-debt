6be1c0e01edd3d00a47a127f2df959d9537df5ab
Carlos Vinicius <cvmm321@gmail.com>
2021-08-09 23:04:21 -0300

    Add function to test saving debtor's registry

--- tests/DebtorTest.php
+++ tests/DebtorTest.php
@@ -16,23 +16,7 @@ class DebtorTest extends TestCase
      */
     public function testDebtorSaveGeneral()
     {
-        $aDados = [
-            'name' => 'Carlos Vinicius',
-            'email' => 'cvmm321@gmail.com',
-            'cpfcnpj' => '01234567890',
-            'birthdate' => '12/01/1994',
-            'phone_number' => '(79) 9 9999-9999',
-            'zipcode' => '99999-999',
-            'address' => 'Rua Deputado Teste',
-            'number' => '25',
-            'complement' => 'Conjunto Teste',
-            'neighborhood' => 'Bairro Teste',
-            'city' => 'Aracaju',
-            'state' => 'SE'
-        ];
-
-        $oDebtor = Debtor::createFromRequest($aDados);
-        $oDebtor->save();
+        $this->saveDebtor();
 
         $oDebtorDAO = new DebtorDAO();
         $oDebtor = $oDebtorDAO->findByCpfCnpj('01234567890');
@@ -357,5 +341,32 @@ class DebtorTest extends TestCase
 
         $this->expectException(InvalidAttributeException::class);
         $oDebtor = Debtor::createFromRequest($aDados);
+    }
+
+    /**
+     * Save debtor registry
+     * @return Debtor
+     * @throws InvalidAttributeException
+     */
+    public function saveDebtor(): Debtor
+    {
+        $aDados = [
+            'name' => 'Carlos Vinicius',
+            'email' => 'cvmm321@gmail.com',
+            'cpf_cnpj' => '01234567890',
+            'birthdate' => '12/01/1994',
+            'phone_number' => '(79) 9 9999-9999',
+            'zipcode' => '99999-999',
+            'address' => 'Rua Deputado Teste',
+            'number' => '25',
+            'complement' => 'Conjunto Teste',
+            'neighborhood' => 'Bairro Teste',
+            'city' => 'Aracaju',
+            'state' => 'SE'
+        ];
+
+        $oDebtor = Debtor::createFromRequest($aDados);
+        $oDebtor->save();
+        return $oDebtor;
     }
 }
